89e79c2c1c6788c39dbb4ddd04b6bce5d244723c
rahul jha <irahul.jha.007@gmail.com>
2025-03-23 01:18:02 +0530

    made somechanges about who won the game

--- src/TicTacToeGame/Board.jsx
+++ src/TicTacToeGame/Board.jsx
@@ -1,32 +1,61 @@
-import React ,{useState}from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 
-const Board = () =>{
-  const[state,setState]=useState(Array(9).fill(null));
-  const[isXTurn,setIsXTurn] = useState(true);
-  const handelClick = (index) =>{
-    const copyState=[...state];
-    copyState[index] =isXTurn ? "X" : "O";
+const Board = () => {
+  const [state, setState] = useState(Array(9).fill(null));
+  const [isXTurn, setIsXTurn] = useState(true);
+  const checkWinner = () => {
+    const winnerLogic = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let logic of winnerLogic) {
+      const [a, b, c] = logic;
+      if (state[a] !== null && state[a] === state[b] && state[a] === state[c]) {
+        return state[a];
+      }
+    }
+    return false;
+  };
+
+  const isWinner = checkWinner();
+
+  const handelClick = (index) => {
+    const copyState = [...state];
+    copyState[index] = isXTurn ? 'X' : 'O';
     setState(copyState);
     setIsXTurn(!isXTurn);
   };
-    return(
-    <div className = 'board-container'>
-    <div className="board-row">
-    <Square onClick={()=> handelClick(0)} value={state[0]}/>
-    <Square onClick={()=> handelClick(1)} value={state[1]}/>
-    <Square onClick={()=> handelClick(2)} value={state[3]}/>
-    </div>
-    <div className="board-row">
-    <Square onClick={()=> handelClick(3)} value={state[3]}/>
-    <Square onClick={()=> handelClick(4)} value={state[4]}/>
-    <Square onClick={()=> handelClick(5)} value={state[5]}/>
-    </div>
-    <div className="board-row">
-    <Square onClick={()=> handelClick(6)} value={state[6]}/>
-    <Square onClick={()=> handelClick(7)} value={state[7]}/>
-    <Square onClick={()=> handelClick(8)} value={state[8]}/>
-    </div>
+
+  return (
+    <div className='board-container'>
+      {isWinner ? (
+        <>{isWinner} won the game!</>
+      ) : (
+        <>
+          <div className='board-row'>
+            <Square onClick={() => handelClick(0)} value={state[0]} />
+            <Square onClick={() => handelClick(1)} value={state[1]} />
+            <Square onClick={() => handelClick(2)} value={state[2]} />
+          </div>
+          <div className='board-row'>
+            <Square onClick={() => handelClick(3)} value={state[3]} />
+            <Square onClick={() => handelClick(4)} value={state[4]} />
+            <Square onClick={() => handelClick(5)} value={state[5]} />
+          </div>
+          <div className='board-row'>
+            <Square onClick={() => handelClick(6)} value={state[6]} />
+            <Square onClick={() => handelClick(7)} value={state[7]} />
+            <Square onClick={() => handelClick(8)} value={state[8]} />
+          </div>
+        </>
+      )}
     </div>
   );
 };
